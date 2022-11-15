19ce3ff6268246e3df29fe5fa39d0b8095e434b8
lily <lceciledesign@gmail.com>
2022-11-15 19:13:08 +0100

    Cocktail finder: Add endpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CocktailMainComponent } from './cocktail-main/cocktail-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CocktailMainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
