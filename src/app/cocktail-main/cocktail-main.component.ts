c9fe05f9530fa490b43c012d98e310f1fa070c20
lily <lceciledesign@gmail.com>
2022-11-17 18:15:50 +0100

    Cocktail picker * Images endpoint added

--- src/app/cocktail-main/cocktail-main.component.ts
+++ src/app/cocktail-main/cocktail-main.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { map, Subject, Subscription, switchMap } from 'rxjs';
 import { CocktailService } from 'src/services/cocktail.service';
 
 @Component({
@@ -6,14 +7,69 @@ import { CocktailService } from 'src/services/cocktail.service';
   templateUrl: './cocktail-main.component.html',
   styleUrls: ['./cocktail-main.component.less']
 })
-export class CocktailMainComponent implements OnInit {
-
+export class CocktailMainComponent implements OnInit, OnDestroy {
+  searchValue: Subject<string> = new Subject<string>();
+  searchValueSubscription: Subscription;
+  ingredient: string;
+  ingredientsList: Array<any> = [];
+  drinksArray: Array<any> = [];
+  cocktailList: any;
+  imageUrl: any;
   constructor(private readonly cocktailService: CocktailService) { }
 
   ngOnInit() {
-    this.cocktailService.getCocktails('Gin').subscribe(result => {
-      console.log(result, 'result');
+    this.cocktailService.getImages('gin').subscribe(result => {
+      this.createImageFromBlob(result);
+      this.imageUrl = result;
     })
+    this.findIngredients();
+    this.searchValueSubscription = this.searchValue.pipe(
+      map((value: string) => {
+        this.ingredient = value;
+      }),
+      //   switchMap(() => this.getCocktails(this.ingredient));
+      //   this.cocktailService.getCocktails(this.ingredient))
+      // ).subscribe((data: any) => {
+      //   console.log(data, 'data')
+      // });
+    ).subscribe()
+  }
+
+  createImageFromBlob(image: Blob) {
+    let reader = new FileReader();
+    reader.addEventListener("load", () => {
+      this.imageUrl = reader.result;
+    }, false);
+
+    if (image) {
+      reader.readAsDataURL(image);
+    }
+  }
+
+
+
+  getCocktails(value: any) {
+    return this.cocktailService.getCocktails(value).subscribe((data: any) => {
+      this.cocktailList = data;
+    })
+  }
+
+  findCocktail(search: any): void {
+    this.searchValue.next(search.target.value);
+  }
+
+  findIngredients(): void {
+    this.cocktailService.getIngredients().subscribe(ingredients => {
+      // this.ingredientsList.push(ingredients.drinks);
+      // console.log(this.ingredientsList)
+      this.ingredientsList = ingredients.drinks;
+      console.log(this.ingredientsList, 'list')
+
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.searchValueSubscription.unsubscribe();
   }
 
 }
--- src/services/cocktail.service.ts
+++ src/services/cocktail.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CocktailService {
-  url: string = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i='
+  url: string = 'https://www.thecocktaildb.com/api/json/v1/1/'
+  images: string = 'https://www.thecocktaildb.com/images/ingredients/'
+
+  alcoholFilter: string = 'filter.php?i=';
+  ingredients: string = 'list.php?i=list';
 
   constructor(private http: HttpClient) { }
-  // https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
 
-  getCocktails(alcohol: string) {
-    return this.http.get(`${this.url}${alcohol}`);
+  getCocktails(alcohol: string): Observable<any> {
+    return this.http.get(`${this.url}${this.alcoholFilter}${alcohol}`);
+  }
+
+  getIngredients(): Observable<any> {
+    return this.http.get(`${this.url}${this.ingredients}`)
+  }
+
+  getImages(ingredient: string): Observable<any> {
+    return this.http.get(`${this.images}${ingredient}-Medium.png`, { responseType: 'blob' });
   }
 }
